8f9ef82ce28e7f5ee4b1427976b673c8eda4a804
Kokonod Ray <kokonodray2001@gmail.com>
2023-07-11 22:26:28 +0530

    update

--- README.md
+++
@@ -1,2 +0,0 @@
-# Ecom-MarketPlace
-Name- Ecom-MarketPlace , this an website to shop from your nearby local store
--- Server/authApi/connections/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql";
-const db = mysql.createConnection({
-    host : process.env.host,
-    port :"3306",
-    user : process.env.user,
-    password: process.env.password+"##++",
-    database: process.env.database
-})
-export default db;
